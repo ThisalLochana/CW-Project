95771c37f36bcb3c8b8fc92477841ebf2b528641
Thisal Lochana <134125776+ThisalLochana@users.noreply.github.com>
2024-03-26 00:48:41 +0530

    Initial commit

---
+++ Assets/Sathnidu/Scripts/CapsuleLock.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsulelock : MonoBehaviour
+{
+    private bool isLocked = false;
+    private Vector3 lockedPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            isLocked = !isLocked;
+            if (isLocked)
+            {
+                lockedPosition = transform.position;
+            }
+        }
+        if (isLocked)
+        {
+            transform.position = lockedPosition;
+        }
+    }
+}
---
+++ Assets/Thisal/Scripts/CameraControl.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 8.0f;
+
+    private bool isMouseLocked = true; // Indicates whether the mouse is locked
+
+    private void Start()
+    {
+        LockMouseCursor();
+    }
+
+    private void Update()
+    {
+        // Toggle mouse lock with the "Escape" key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMouseLock();
+        }
+
+        // Unlock the mouse cursor with the middle mouse button
+        if (Input.GetMouseButtonDown(2)) // 2 corresponds to the middle mouse button
+        {
+            UnlockMouseCursor();
+        }
+
+        if (isMouseLocked)
+        {
+            // Camera movement
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+            // Camera rotation
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            Vector3 rotation = new Vector3(-mouseY, mouseX, 0) * rotationSpeed;
+            transform.eulerAngles += rotation;
+        }
+    }
+
+    private void ToggleMouseLock()
+    {
+        isMouseLocked = !isMouseLocked;
+        if (isMouseLocked)
+        {
+            LockMouseCursor();
+        }
+        else
+        {
+            UnlockMouseCursor();
+        }
+    }
+
+    private void LockMouseCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void UnlockMouseCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
